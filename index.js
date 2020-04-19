a75da437e6e857a4a7ff35cf856b4fb94e38ef01
William Schwindt <williammschwindt@gmail.com>
2020-04-19 16:29:01 -0700

    server set up

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const port = process.env.PORT || 8000;
 
-Sing along:
+const express = require('express');
+const server = express();
+server.use(express.json());
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+server.get('/', (req, res) => {
+    res.send(`<h1>Node api challenge</h1>`);
+});
 
-Go code!
-*/
+server.use((req, res) => {
+    res.status(404).json({
+        message: 'route not found'
+    })
+});
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+        message: 'something went wrong'
+    })
+});
+
+server.listen(port, () => {
+    console.log(`server running on port ${port}`)
+});
+
+
