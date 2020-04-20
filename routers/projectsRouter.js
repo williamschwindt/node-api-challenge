d7b6450042225348d636a043df105a57d5065850
William Schwindt <williammschwindt@gmail.com>
2020-04-19 17:54:09 -0700

    projects routercomplete

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,37 +1,74 @@
 const express = require('express');
 const projectsDB = require('../data/helpers/projectModel');
+const actionsDB = require('../data/helpers/actionModel');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    projectsDB.get()
+        .then(projects => {
+            return res.status(200).json(projects)
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
-router.get('/:id', (req, res) => {
-    
+router.get('/:id', validateProjectID(), (req, res) => {
+    return res.status(200).json(res.project)
 })
 
-router.get('/:id/actions', (req, res) => {
-    
+router.get('/:id/actions', validateProjectID(), (req, res) => {
+    projectsDB.getProjectActions(req.params.id)
+        .then(actions => {
+            return res.status(200).json(actions)
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
-router.post('/', (req, res) => {
-    
+router.post('/', validateProjectBody(), (req, res) => {
+    projectsDB.insert(req.body)
+        .then(project => {
+            return res.status(201).json(project)
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
-router.post('/:id/actions', (req, res) => {
-    
+router.post('/:id/actions', validateActionBody(), validateProjectID(), (req, res) => {
+    actionsDB.insert(req.body)
+        .then(project => {
+            return res.status(200).json(project)
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
-router.put('/:id', (req, res) => {
-    
+router.put('/:id', validateProjectBody(), validateProjectID(), (req, res) => {
+    projectsDB.update(req.params.id, req.body)
+        .then(project => {
+            return res.status(200).json(project)
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
-router.delete('/:id', (req, res) => {
-    
+router.delete('/:id', validateProjectID(), (req, res) => {
+    projectsDB.remove(req.params.id)
+        .then(() => {
+            return res.status(204).end()
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
-const validateProjectID = () => {
+function validateProjectID() {
     return (req, res, next) => {
         projectsDB.get(req.params.id)
             .then(project => {
@@ -50,9 +87,9 @@ const validateProjectID = () => {
     }
 }
 
-const validateProjectBody = () => {
+function validateProjectBody() {
     return (req, res, next) => {
-        if(!res.body.name || !res.body.description) {
+        if(!req.body.name || !req.body.description) {
             return res.status(400).json({
                 message: 'request body is missing name or description'
             })
@@ -61,14 +98,14 @@ const validateProjectBody = () => {
     }
 }
 
-const validateActionBody = () => {
+function validateActionBody() {
     return (req, res, next) => {
-        if(!res.body.project_id) {
+        if(!req.body.project_id) {
             return res.status(400).json({
                 message: 'request body is missing project_id'
             })
         }
-        if(!res.body.description || !res.body.notes) {
+        if(!req.body.description || !req.body.notes) {
             return res.status(400).json({
                 message: 'request body is missing description or notes'
             })
